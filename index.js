beac2f0da7af664db23a09f865295e8cbf8180fc
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-05-16 13:05:49 +0530

    .

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+variables : store the value in memory location
+
+Data : raw facts - smallest entity of any information
+
+Information : Structured data collection
+
+Data type : 
+string
+
+*/
+
+// let x = 65;
+
+// let age = prompt("Enter your age : ");
+
+// if(age>=18)
+// {
+//     console.log("You are eligible to vote");
+// }
+// else{
+//     console.log("You have to wait for vote");
+// }
+
+// let nm = "Hello World";
+
+// console.log(typeof(nm));
+
+
+// let nm = 'Uqba';
+// if(nm == 'Uqba')
+// {
+//     console.log("Your gender is male");
+// }
+// else
+// {
+//     console.log("You are learning Node js");
+// }
+
+
+// let nm = prompt("Enter your name : ");
+// if(nm == 'uqba')
+// {
+//     console.log("Hello Uqba");
+// }
+// else{
+//     console.log("Hello React");
+// }
+
+// let nm = "Hello React";
+
+// let res = nm.toLowerCase();
+// console.log(res);
+
+
+// let nm = prompt("Enter your name : ");
+// nm = nm.toLowerCase();
+// if(nm == 'uqba')
+// {
+//     console.log("Hello Uqba");
+// }
+// else{
+//     console.log("Hello React");
+// }
