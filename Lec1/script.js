90f6ed0488230fc98f1b2dd9737af7c26ed3aac4
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-05-18 13:01:14 +0530

    .

---
+++ Lec1/script.js
@@ -0,0 +1,119 @@
+/*
+Variables : 
+used to store the value in memory location
+
+Keywords:
+let - 
+const - 
+var -
+
+
+
+Data types : 
+Number : 
+String : 
+BigInt : 
+Boolean : true and False
+Null :
+
+
+Object : 
+operators : 
+
+
+
+*/
+// var x = 50; //global scoped
+// {
+//     var x = 60;
+//     console.log(x);
+// }
+// console.log(x);
+
+//ES 2015 : let and const
+
+
+// let x = 50; //blocked
+// {
+//     let x = 60;
+//     console.log(x);
+// }
+// console.log(x);
+
+
+// const x = 50; //blocked scope
+// {
+//     const x = 60;
+//     console.log(x);
+// }
+// console.log(x);
+
+//What is the difference between let and const?
+
+
+// let x = 50;
+// x = 60;
+// console.log(x);
+
+// const x = 50;
+// x = 60;
+// console.log(x);
+
+// const nm = "Uqba";
+
+//Pubg - 
+//Free Fire - 
+//Counter Strike - 
+//Call of Duty - 
+
+// const chances = 5;
+// chances = 10;
+// console.log(chances);
+
+
+// let nm = 45;
+// console.log(typeof(nm));
+
+
+// let nam = "Uqba";
+// console.log(nam);
+// console.log(typeof(nam));
+
+
+// let dt = 4564641315464646464646464646464646;
+// console.log(typeof(dt));
+// console.log(dt);
+
+// let num = BigInt(dt);
+// console.log(num);
+
+
+
+// let dt = 4564641315464646464646464646464646n;
+// console.log(typeof(dt));
+
+
+// let t = true;
+// console.log(typeof(t));
+
+
+// let f = false ; 
+// console.log(typeof(f));
+
+
+
+// let fruits = ["Apple", "Orange", "PineApple", "Grapes", "Mango"];
+// console.log(typeof(fruits));
+
+
+// let data = {
+//     name : "Uqba",
+//     age : 21,
+//     course : 'AI'
+// };
+
+// console.log(typeof(data));
+
+
+// let num = [1,52,24,26,27,28];
+// console.log(typeof(num));
